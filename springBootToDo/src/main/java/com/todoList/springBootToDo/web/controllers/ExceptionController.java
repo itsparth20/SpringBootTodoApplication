0ed9527020a5e0161500f4742ac0b780b80b6d6d
Parth <itsparth20@gmail.com>
2017-09-11 21:55:12 -0700

    Added Error Exception and Default Error page

---
+++ springBootToDo/src/main/java/com/todoList/springBootToDo/web/controllers/ExceptionController.java
@@ -0,0 +1,18 @@
+package com.todoList.springBootToDo.web.controllers;
+
+//@Controller("error")
+public class ExceptionController {
+
+//    private Log logger = LogFactory.getLog(ExceptionController.class);
+//
+//    @ExceptionHandler(Exception.class)
+//    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+//        logger.error("Request: " + req.getRequestURL() + " raised " + ex);
+//
+//        ModelAndView mav = new ModelAndView();
+//        mav.addObject("exception", ex);
+//        mav.addObject("url", req.getRequestURL());
+//        mav.setViewName("error");
+//        return mav;
+//    }
+}
